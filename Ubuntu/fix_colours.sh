de3ac233ef07ed328361b6ad2b23734de709acaa
Alfredo Canziani <alfredo.canziani@gmail.com>
2015-10-09 17:57:18 -0400

    New colour set for Ubuntu

--- Ubuntu/fix_colours.sh
+++ Ubuntu/fix_colours.sh
@@ -1,13 +1,6 @@
 # Fix terminal colours
 gconftool-2 -s -t bool   /apps/gnome-terminal/profiles/Default/use_theme_background false
 gconftool-2 -s -t bool   /apps/gnome-terminal/profiles/Default/use_theme_colors false
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/background_color '#000000000000'
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/foreground_color '#F2F2F2F2F2F2'
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/palette          '#202020202020:#5D5D1A1A1414:#42424E4E2424:#6F6F50502828:#26263E3E4E4E:#3E3E1F1F5050:#23234E4E3F3F:#979797979797:#55555555:#DADA49493939:#A5A5C2C26161:#FFFFC6C66D6D:#6D6D9C9CBEBE:#A2A25656C7C7:#6262C1C1A1A1:#FFFFFFFFFFFF'
-
-# Fix ls colours
-cd
-wget https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.256dark
-mv dircolors.256dark .dircolors
-eval `dircolors ~/.dircolors`
-cd -
+gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/background_color '#121212121212'
+gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/foreground_color '#c5c5c8c8c6c6'
+gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/palette          '#28282a2a2e2e:#8c8c38383838:#73737a7a3535:#c4c483835454:#51516d6d8585:#6d6d54547575:#4d4d73736e6e:#59595f5f6666:#37373b3b4141:#cccc66666666:#b5b5bdbd6868:#f0f0c6c67474:#8181a2a2bebe:#b2b29494bbbb:#8a8abebeb7b7:#c5c5c8c8c6c6'
