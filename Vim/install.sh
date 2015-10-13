ac342dae34e35b13e6b676fb47c6a0761fdeb190
Alfredo Canziani <alfredo.canziani@gmail.com>
2015-10-13 00:33:37 -0400

    Bugfix introduced with 11af496

--- Vim/install.sh
+++ Vim/install.sh
@@ -6,6 +6,6 @@ mkdir vim/bundle
 git clone --quiet https://github.com/VundleVim/Vundle.vim.git vim/bundle/Vundle.vim
 ln -s $(pwd)/vimrc $HOME/.vimrc
 ln -s $(pwd)/vim   $HOME/.vim
-vim -u vim/myVundle.vim +PluginInstall +qall
+vim -u vim/myBundle.vim +PluginInstall +qall
 
 echo 'Done.'
