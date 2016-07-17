e16e52c75a44046b0d14035fe6573bb8992a59c2
Alfredo Canziani <alfredo.canziani@gmail.com>
2016-07-17 15:52:26 -0400

    Add tmux-install for Ubuntu

---
+++ Ubuntu/tmux-install.sh
@@ -0,0 +1,4 @@
+sudo apt-get install -y python-software-properties software-properties-common
+sudo add-apt-repository -y ppa:pi-rho/dev
+sudo apt-get update
+sudo apt-get install -y tmux=2.0-1~ppa1~t
