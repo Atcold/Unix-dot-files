aadb5a79b0427e7d63e1e9a43db78ab410dc47c2
Alfredo Canziani <alfredo.canziani@gmail.com>
2017-10-26 15:19:38 -0400

    Conditional Tmux conf for Mac

--- Tmux/conf_settings.sh
+++ Tmux/conf_settings.sh
@@ -7,4 +7,10 @@ tic -x tmux.terminfo
 rm -rf ~/.tmux.conf
 ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
 
+# If on Mac, get also Mac specific settings
+if [[ $(uname) == 'Darwin' ]]; then
+    ln -s $(pwd)/tmux.mac.conf $HOME/.tmux.mac.conf
+fi
+
+
 echo 'Done.'
