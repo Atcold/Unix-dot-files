5d883ae15468a5a4f0d2438402aa98ba7e6b1d94
Alfredo Canziani <alfredo.canziani@gmail.com>
2017-10-25 21:15:08 -0400

    Remove broken fix_colours...

--- Ubuntu/install.sh
+++ Ubuntu/install.sh
@@ -4,6 +4,6 @@ echo ''                            >> ~/.bashrc
 echo '# My configurations'         >> ~/.bashrc
 echo source $(pwd)/bashrcAppend.sh >> ~/.bashrc
 ln -s $(pwd)/dircolors.monokai $HOME/.dir_colors
-./fix_colours.sh
+#./fix_colours.sh
 
 echo 'Done.'
