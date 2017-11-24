e64dd91911f9c9857a1362344cd6775c742c2983
Alfredo Canziani <alfredo.canziani@gmail.com>
2017-11-24 16:54:15 -0500

    Install reattach-to-user-namespace on Mac

--- Tmux/conf_settings.sh
+++ Tmux/conf_settings.sh
@@ -11,6 +11,7 @@ ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
 # If on Mac, get also Mac specific settings
 if [[ $(uname) == 'Darwin' ]]; then
     ln -s $(pwd)/tmux.mac.conf $HOME/.tmux.mac.conf
+    brew install reattach-to-user-namespace
 fi
 
 
