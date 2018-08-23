302c08cc117fe71b467babd72ad71d635badec73
Alfredo Canziani <alfredo.canziani@gmail.com>
2018-08-23 14:44:39 -0400

    Update MacOS configurations

--- Mac/install.sh
+++ Mac/install.sh
@@ -1,3 +1,11 @@
+echo 'Installing essential command line tools'
+
+brew cask install osxfuse
+brew install unrar coreutils pdfgrep sshfs bash-completion rename htop wget
+
+echo 'Done.'
+
+
 echo 'Installing Bash configurations'
 
 rm -rf $HOME/.bashrc
@@ -5,14 +13,6 @@ ln -s $(pwd)/bashrc $HOME/.bashrc
 
 # Get decent ls colours
 wget "https://github.com/trapd00r/LS_COLORS/raw/master/LS_COLORS" -O $HOME/.dir_colors
-
-echo 'Done.'
-
-
-echo 'Installing essential command line tools'
-
-brew cask install osxfuse
-brew install unrar coreutils pdfgrep sshfs
 
 echo 'Done.'
 
