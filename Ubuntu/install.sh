4e5bef74a0239fcedec25ff0e24515f1781d0eb8
Alfredo Canziani <alfredo.canziani@gmail.com>
2018-11-26 15:41:00 -0500

    Fix LSCOLOURS for Linux

--- Ubuntu/install.sh
+++ Ubuntu/install.sh
@@ -3,7 +3,8 @@ echo 'Installing Bash configurations'
 echo ''                            >> ~/.bashrc
 echo '# My configurations'         >> ~/.bashrc
 echo source $(pwd)/bashrcAppend.sh >> ~/.bashrc
-ln -s $(pwd)/dircolors.monokai $HOME/.dir_colors
-#./fix_colours.sh
+URL=https://raw.github.com/trapd00r/LS_COLORS/master/LS_COLORS
+wget $URL -O $HOME/.dir_colors
+# ./fix_colours.sh ???
 
 echo 'Done.'
