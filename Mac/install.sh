9345273d595efc8dffaaa0444185bcd682880cf2
Alfredo Canziani <alfredo.canziani@gmail.com>
2023-03-10 15:15:26 -0500

    Add tree to default brew installation

--- Mac/install.sh
+++ Mac/install.sh
@@ -1,6 +1,6 @@
 echo 'Installing essential command line tools'
 
-brew install git coreutils pdfgrep bash-completion rename htop wget miniconda ranger
+brew install git coreutils pdfgrep bash-completion rename htop wget miniconda ranger tree
 brew install visual-studio-code rectangle keepingyouawake spotify vlc stats
 
 echo 'Done.'
