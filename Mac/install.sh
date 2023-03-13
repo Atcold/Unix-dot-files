fea414d3e521477f8eabdbcb7597a62c3bb3087c
Alfredo Canziani <alfredo.canziani@gmail.com>
2023-03-13 16:02:30 -0400

    Adding rclone to Mac installation

--- Mac/install.sh
+++ Mac/install.sh
@@ -1,6 +1,6 @@
 echo 'Installing essential command line tools'
 
-brew install git coreutils pdfgrep bash-completion rename htop wget miniconda ranger tree
+brew install git coreutils pdfgrep bash-completion rename htop wget miniconda ranger tree rclone
 brew install visual-studio-code rectangle keepingyouawake spotify vlc stats
 
 echo 'Done.'
