3c1afbfc42e26e7594bc0bc18704a9c823519386
Alfredo Canziani <alfredo.canziani@gmail.com>
2026-05-19 15:27:41 +0200

    Mac: add fzf to install.sh, bashrc uses fzf --bash

--- Mac/install.sh
+++ Mac/install.sh
@@ -1,6 +1,6 @@
 echo 'Installing essential command line tools'
 
-brew install bash git coreutils pdfgrep bash-completion rename htop wget miniconda ranger tree rclone gh
+brew install bash git coreutils pdfgrep bash-completion rename htop wget miniconda ranger tree fzf rclone gh
 brew install visual-studio-code rectangle keepingyouawake spotify vlc stats avidemux languagetool
 brew tap hamed-elfayome/claude-usage
 brew install claude-usage-tracker mac-mouse-fix
